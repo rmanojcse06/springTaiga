cc14af1ff7ae2d4e94e90d21b69bce88836ebc8c
Manoj Ravikumar <manoj.ravikumar@hpe.com>
2024-10-25 07:16:27 +0530

    Feat: Fixfor login page in loop

--- ttm/src/main/java/com/kpl/ttm/Controller/ContentController.java
+++ ttm/src/main/java/com/kpl/ttm/Controller/ContentController.java
@@ -2,13 +2,14 @@ package com.kpl.ttm.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class ContentController {
 
-   @GetMapping("/login")
-    public String login() {
+    @GetMapping("/")
+    public String showIndex() {
         System.out.println("login successfully!!!");
         return "loginPage";
     }
--- ttm/src/main/java/com/kpl/ttm/Security/SecurityConfig.java
+++ ttm/src/main/java/com/kpl/ttm/Security/SecurityConfig.java
@@ -9,22 +9,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-   
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
-         .formLogin(httpForm -> { 
-            httpForm
-               .loginPage("/login").permitAll();
-            
-         })
-
-            .authorizeHttpRequests(registry ->{
-                registry.requestMatchers("/req/signup", "/css/**", "/js/**").permitAll(); // Allow signup
+            .formLogin(httpForm -> { 
+                httpForm
+                    .loginPage("/").permitAll(); // Custom login page
+            })
+            .authorizeHttpRequests(registry -> {
+                registry.requestMatchers("/req/signup", "/login", "/css/**", "/js/**", "/img/**").permitAll(); // Allow static resources
                 registry.anyRequest().authenticated(); // All other requests require authentication
-      })
-
-      .build(); // Build the security filter chain
-   }
+            })
+            .build();
+    }
 }
